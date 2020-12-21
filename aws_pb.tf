a90081eb0d6e5441f01b831de3badd77a09868ec
GendalfGV <ggolomzin@yandex.ru>
2020-12-21 12:13:44 +0300

    fff

--- aws_pb.tf
+++ aws_pb.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
 variable "aws-instance-type" {
   default = "t2.micro"
-  description = "EC2 acces key pair"
+  description = "type distr"
 }
 variable "aws-ami" {
     default="ami-0dd9f0e7df0f0a138"
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "developer" {
     ami = "${var.aws-ami}"
     instance_type = "${var.aws-instance-type}"
-    vpc_security_group_ids = aws_security_group.DevopsSG.id
+    vpc_security_group_ids = [aws_security_group.DevopsSG.id]
     user_data=<<EOF
 #!/bin/bash
 apt update
@@ -22,25 +22,32 @@ apt install default-jdk -y
 apt install git -y
 apt install maven -y
 apt install awscli - y
-git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git /home/user/boxfuse 
+git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git /home/user/boxfuse
 cd /home/user/boxfuse
 mvn package
-aws s3 sync /home/user/boxfuse/target/hello-1.0.war DevopsBucket21122020
+aws s3 sync /home/user/boxfuse/target/hello-1.0.war devopsbucket21122020
     EOF
+
+    tags = {
+    Name = "devops_developer"
+  }
 }
 
 resource "aws_instance" "production" {
     ami = "${var.aws-ami}"
     instance_type = "${var.aws-instance-type}"
-    vpc_security_group_ids = aws_security_group.DevopsSG.id
+    vpc_security_group_ids = [aws_security_group.DevopsSG.id]
     user_data=<<EOF
 #!/bin/bash
 apt update
 apt install default-jdk -y
 apt install tomcat9 -y
 apt install awscli - y
-aws s3 sync DevopsBucket21122020/hello-1.0.war  /var/lib/tomcat9/webapps/ 
+aws s3 sync devopsbucket21122020/hello-1.0.war  /var/lib/tomcat9/webapps/
     EOF
+    tags = {
+    Name = "devops_production"
+  }
 }
 
 resource "aws_security_group" "DevopsSG" {
@@ -57,15 +64,15 @@ resource "aws_security_group" "DevopsSG" {
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
 
-    } 
+    }
 }
 
-resource "aws_s3_bucket" "DevopsBucket21122020" {
-  bucket = "DevopsBucket21122020"
+resource "aws_s3_bucket" "devopsbucket21122020" {
+  bucket = "devopsbucket21122020"
   acl    = "private"
 
   tags = {
-    Name        = "DevopsBucket21122020"
+    Name        = "devopsbucket21122020"
     Environment = "Dev"
   }
 }
